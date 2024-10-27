a4d529ec5bdce640376e7fa201fb6566958571a3
Antoan Metodiev <Boksnem@abv.bg>
2024-10-27 03:09:55 +0200

    create home page who will contain all my functionalities

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -1,12 +1,29 @@
 
+import Header from "./structure/Header/Header";
+import Body from "./structure/Body/Body";
+import { useState } from "react";
 
 
 const HomePage = () => {
+    const [showTasks, setShowTasks] = useState(false);
+    const [specificLectures, setSpecificLectures] = useState([]);
 
+    // Functions:
+    let setSpecificLecturesHandler = (newSpecificLectures) => { setSpecificLectures(newSpecificLectures) }
+    let setSetShowTasksHandler = (value) => { setShowTasks(value) }
 
 
     return (
-        <h1>LMAOO</h1>
+        <article>
+            <Header 
+                setSpecificLecturesHandler={setSpecificLecturesHandler}
+                setSetShowTasksHandler={setSetShowTasksHandler}
+            />
+            <Body 
+                specificLectures={specificLectures}
+                showTasks={showTasks}
+             />
+        </article>
     )
 }
 
