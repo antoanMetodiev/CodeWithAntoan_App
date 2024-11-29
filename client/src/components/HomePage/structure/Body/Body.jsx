04f58fd331d94e4a70817009307b9d87d1f859ce
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-11-29 17:20:46 +0200

    Update Body.jsx

--- client/src/components/HomePage/structure/Body/Body.jsx
+++ client/src/components/HomePage/structure/Body/Body.jsx
@@ -13,13 +13,6 @@ const Body = ({
     const [currentWatchingLecture, setCurrentWatchingLecture] = useState({});
     const [isWatchLecture, setIsWatchLecture] = useState(false);
 
-
-
-    // This is for just a test (branch, pullRequest, merge) and will be deleted after one moment!!!
-    const [forDelete, setForDelete] = useState({random: "randomName"});
-
-
-
     // Functions:
     const setIsWatchLectureHandler = (value) => { setIsWatchLecture(value) }
     const setCurrentWatchingLectureHandler = (currentLecture) => { setCurrentWatchingLecture(currentLecture) }
